6f991d35a8439396cb34ab5e48d9c77b055ee76e
vidaimi12 <vidaimi12@gmail.com>
2018-10-28 17:58:18 +0100

    New function added to calculate battery percentage

--- libraries/NaN1_battery/NaN1_battery.h
+++ libraries/NaN1_battery/NaN1_battery.h
@@ -11,7 +11,7 @@ class NaN1_battery
   uint32_t read_bat_voltage(uint32_t averaging_number = 4); //return battery voltage in mV
   bool     get_power_source();                                  //return true if battery return false if usb
   uint8_t  get_battery_status();  //return 0-5 0 empty 4 fully charged 5 usb power
-	
+  uint8_t  get_battery_level(); //return 0-100(%)
 	
 };
 
